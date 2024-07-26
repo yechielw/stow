e906738c3c5a2697ffe63d5fdbd045f676f62d23
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2024-07-26 15:43:07 +0300

    modifed zsh, remvoed old alacritty

--- zsh/.config/zsh/evals.zsh
+++ zsh/.config/zsh/evals.zsh
@@ -1,4 +1,4 @@
 eval "$(zoxide init zsh)"
 eval "$(fzf --zsh)"
-eval "$(atuin init --disable-up-arrow zsh)"
-source  <(atuin gen-completions --shell zsh)
+eval "$(atuin init --disable-up-arrow zsh 2> /dev/null)" 
+source  <(atuin gen-completions --shell zsh 2> /dev/null) 
