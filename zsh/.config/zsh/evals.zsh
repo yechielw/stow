a16bd3247cae608824cc657d30038802c7fae8ab
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2024-07-28 00:24:19 +0300

    added evals (pyenv)

--- zsh/.config/zsh/evals.zsh
+++ zsh/.config/zsh/evals.zsh
@@ -2,3 +2,5 @@ eval "$(zoxide init zsh)"
 eval "$(fzf --zsh)"
 eval "$(atuin init --disable-up-arrow zsh)"
 source  <(atuin gen-completions --shell zsh)
+eval "$(pyenv init -)"
+#eval "$(pyenv virtualenv-init -)"
