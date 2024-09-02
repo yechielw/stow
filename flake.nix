75a23d78a1417e63e89bcccf032048e0bf7eb24d
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2024-09-02 22:51:44 +0300

    flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+
+{
+  description = "My first System + Home-manager flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}
